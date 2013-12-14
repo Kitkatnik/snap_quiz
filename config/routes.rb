cc5dcd6be567e2b7721c6dcad46b103492b13446
Josef Diago <josef.diago@gmail.com>
2013-12-14 17:33:26 -0500

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,47 +5,7 @@ SnapQuiz::Application.routes.draw do
   resources :students
 
   get '/metrics' => 'metrics#index'
-  get '/metrics/question/:id' => 'metrics#question'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  get '/metrics/question/:id' => 'metrics#question', as: 'chart'
 
   get '/quiz/:id' => 'quiz#show', as: 'quiz'
   post '/quiz/:id' => 'quiz#answer', as: 'answer'
