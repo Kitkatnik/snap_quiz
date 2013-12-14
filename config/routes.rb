7080637a447c1841ff84905efbd1d829974b5664
Katya Sarmiento <Kat@Katyas-MacBook-Pro.local>
2013-12-14 00:00:49 -0500

    added bootstrap and created students and index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SnapQuiz::Application.routes.draw do
   resources :questions
-
+  resources :students
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
