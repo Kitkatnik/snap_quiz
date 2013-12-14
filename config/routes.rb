3aab89bc47ebac2517804780d7b992302f16374e
Josef Diago <josef.diago@gmail.com>
2013-12-14 15:33:02 -0500

    routes for various things

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ SnapQuiz::Application.routes.draw do
   resources :students
 
   get '/quiz/:id' => 'quiz#show', as: 'quiz'
+  post '/quiz/:id' => 'quiz#answer', as: 'answer'
   post '/quiz/new/:student' => 'quiz#new', as: 'new_quiz'
+
+  get '/you_win' => 'quiz#win'
+  get '/you_fail' => 'quiz#fail'
 end
