f40e135290aca0fb8a81a6c321fca7dce8b0c3c7
Katya Sarmiento <Kat@katyas-mbp.lan>
2013-12-13 19:53:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SnapQuiz::Application.config.secret_key_base = 'fb92784492cb805eb0c143e2e6af287712b0c8fb33254842ec922482dd5b0e5844e3d7aeec84686912c02c47c39eb1003b45791c77a1945a656a390a28827b82'
