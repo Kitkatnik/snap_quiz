1b36647e3df53f01b428789db8331ec8d270c3ee
Josef Diago <josef.diago@gmail.com>
2013-12-14 15:32:35 -0500

    check answer and redirect

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -13,4 +13,22 @@ class QuizController < ApplicationController
   def show
     @quiz = Quiz.find params[:id]
   end
+
+  def answer
+    quiz = Quiz.find params[:id]
+    quiz.response = params[:answer]
+    quiz.save
+
+    if quiz.question.answer == quiz.response
+      redirect_to '/you_win'
+    else
+      redirect_to '/you_fail'
+    end
+  end
+
+  def win
+  end
+
+  def fail
+  end
 end
