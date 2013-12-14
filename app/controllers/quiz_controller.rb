6d771a4b1556795ce72bcd53516655fe6548f6b6
Josef Diago <josef.diago@gmail.com>
2013-12-14 12:11:14 -0500

    new quiz action

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,2 +1,12 @@
 class QuizController < ApplicationController
+  def new
+    student = Student.find params[:student]
+
+    quiz = Quiz.new
+    quiz.student = student
+    quiz.question = Question.first
+    quiz.save
+
+    redirect_to students_path
+  end
 end
